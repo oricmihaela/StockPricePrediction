91050f6b0ef58063a38e0cbaade7f0f9638c697a
Mihaela Orić <oricmihaela@gmail.com>
2021-07-16 21:46:49 +0200

    commit4

--- stock_price_prediction.py
+++ stock_price_prediction.py
@@ -49,6 +49,7 @@ plot = go.Figure(data = tesla_data, layout = layout)
 #plot(plot) plotting offline
 iplot(plot)
 
+from sklearn.linear_model import LinearRegression
 #building the regression model
 from sklearn.model_selection import train_test_split
 #for preprocessing
@@ -58,7 +59,16 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error as mse
 from sklearn.metrics import r2_score
 
+#split the data into train and test set
+X = np.array(tesla.index).reshape(-1, 1)
+y = tesla['Close']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 42)
 
+#feature scaling
+scaler = StandardScaler().fit(X_train)
 
+#creating a linear model
+lm = LinearRegression()
+lm.fit(X_train, y_train)
 
 print('Code sueceeded')
